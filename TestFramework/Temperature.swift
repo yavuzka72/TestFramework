9f33bd1a91bea829c0f690396f0b63cb0c02c72c
Yavuz Karakuş <yavuz.karakus72@gmail.com>
2021-04-09 10:52:58 +0300

    güncelleme yaptım

--- TestFramework/Temperature.swift
+++ TestFramework/Temperature.swift
@@ -12,10 +12,10 @@ public class Temperature{
     public var calsius: Float = 0.0
     public  var fahren:Float {
         get{
-            return calsius + 12.0
+            return (calsius * 1.8) + 32.0
         }
         set{
-             calsius = calsius + 12
+            calsius = (newValue - 32) / 1.8
         }
         
     }
